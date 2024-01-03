e5b867168ebb886acacbfaabcc1669c484b44800
Jinwoo Lee <binaryWooo@gmail.com>
2024-01-03 23:51:38 +0900

    feat: define develop vpc #4

--- main.tf
+++ main.tf
@@ -1,10 +1,11 @@
 /*
 ** 개발 환경
 */
-resource "ncloud_vpc" "vpc" {
+resource "ncloud_vpc" "develop_vpc" {
   name            = "${var.zone_name}-${var.terraform_name}-${var.develop_env_name}-vpc"
   ipv4_cidr_block = var.vpc_develop_cidr
 }
+
 
 /*
 ** 운영 환경
