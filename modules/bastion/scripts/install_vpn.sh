c4d29b4e3bcac2957c21e6f7ccac6dbb1b369130
Jinwoo Lee <jinlee1703@gmail.com>
2024-09-06 12:28:24 +0900

    VPN 미동작 이슈 해결 (#24)
    * fix: vpn을 위한 포트 허용 #23
    
    * fix: dns 서버 설정을 위한 private_ip 수정 #23

--- modules/bastion/scripts/install_vpn.sh
+++ modules/bastion/scripts/install_vpn.sh
@@ -6,7 +6,7 @@ sudo apt-get install -y docker.io docker-compose
 
 # 환경 변수 설정
 export PUBLIC_IP=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
-export PRIVATE_IP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
+export PRIVATE_IP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4 | sed 's/\.[0-9]*$/.2/')
 
 # OpenVPN 서비스 시작
 sudo -E docker-compose up -d
